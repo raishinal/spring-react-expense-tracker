ed3b1aea71e0b01c96f92223aed5f42efa0de857
raishinal <roslmggac@gmail.com>
2019-11-07 09:53:07 +0545

    complete

--- expense/src/main/java/com/example/expense/expense/model/Expense.java
+++ expense/src/main/java/com/example/expense/expense/model/Expense.java
@@ -7,6 +7,8 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,13 +25,14 @@ public class Expense {
 	
 	private Instant expensedate;
 	
-	private String descript;
+	private String description;
 	
 	private String location;
 	
 	@ManyToOne
 	private Category category;
 	
+	@JsonIgnore
 	@ManyToOne
 	private User user;
 	
